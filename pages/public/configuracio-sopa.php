725fcfbe9a933882ebe32750e21a2a1c24432026
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-01-22 09:00:29 +0100

    fix configuration form to global style

--- pages/public/configuracio-sopa.php
+++ pages/public/configuracio-sopa.php
@@ -1,9 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
+    $language = $_POST["language"];
     $bgColor = $_POST["backgroundTheme"];
     $buttonColor = $_POST["theme"];
 
+    echo "Llenguatge seleccionat: " . $language . "<br>";
     echo "Color de fons seleccionat: " . $bgColor . "<br>";
     echo "Color de botó seleccionat: " . $buttonColor . "<br>";
 

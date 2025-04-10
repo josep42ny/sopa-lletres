7617c5cdf63951ec29b1922916485198b9e6978b
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-04-10 08:17:04 +0200

    .

--- wordsearch_front/js/script.js
+++ wordsearch_front/js/script.js
@@ -7,3 +7,49 @@ document.querySelector("#sidebarClose").addEventListener("click", _ => {
 document.querySelector("#sidebarOpen").addEventListener("click", _ => {
     sidebar.style.left = "0";
 });
+
+
+const form = document.querySelector('#userSignup');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const entries = formData.entries();
+    let query = 'insert into User (username, password , firstName ,lastName ,familyName ,email, birthday) values (';
+
+    for (const [key, value] of entries) {
+        if (key == 'repeatPassword') {
+            continue;
+        }
+        query += `'${value}', `;
+
+    }
+    query += ');';
+    const lastIndex = query.lastIndexOf(',');
+    query = query.slice(0, lastIndex) + '' + query.slice(lastIndex + 1);
+
+    (async () => {
+        await fetch("http://localhost:3000/daw/" + encodeURIComponent(query));
+    })()
+});
+
+//(async () => {
+//    const query = "select * from User";
+//    const api = await fetch("http://localhost:3000/daw/" + encodeURIComponent(query))
+//    const data = await api.json();
+//
+//    codi_html = "<table border=1>";
+//    for (const dp of data.data) {
+//        for (const [key, value] of Object.entries(dp)) {
+//            codi_html = codi_html + "<tr><td>" + `${key}: ${value}` + "</td></tr>"
+//        }
+//    }
+//
+//    codi_html = codi_html + "</table>";
+//    document.querySelector("#hello").innerHTML = codi_html;
+//})()
+
+//(async () => {
+//    const query = "insert into Language values ('Català')";
+//    await fetch("http://localhost:3000/daw/" + encodeURIComponent(query));
+//})()
--- wordsearch_front/pages/admin/baixa-usuari.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $baixa_usuari1 = $_POST["user-deletes"];
-    echo "L'usuari que se ha donat de baixa és: " . $baixa_usuari1;
-
-}
-?>
